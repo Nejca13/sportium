b65011dda042a7c71b016a7792be1b7759f6b4ea
agusasaad <agusasaad1099@hotmail.com>
2025-01-13 17:49:03 -0300

    se agregan fuente al layout y se importan seccion inicio

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,11 @@
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 import './globals.css'
+import Navbar from './components/Navbar/Navbar'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const montserrat = Montserrat({
   subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  display: 'swap',
 })
 
 export const metadata = {
@@ -42,8 +39,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang='es'>
+      <body className={` ${montserrat.className}`}>
+        <Navbar />
         {children}
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
+import Inicio from './components/Inicio/Inicio'
 import styles from './page.module.css'
 
 export default function Home() {
-  return <div className={styles.page}></div>
+  return (
+    <div className={styles.page}>
+      <Inicio />
+    </div>
+  )
 }
