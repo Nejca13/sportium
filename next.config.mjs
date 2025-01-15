c9339e7522a39a6bd8e578e3d4f152367182be25
Nejca13 <nicolascontreras677@gmail.com>
2025-01-15 14:47:23 -0300

    Add rewrites for documentation and OpenAPI endpoints; update image remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,35 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
+        source: '/docs',
+        destination: 'http://127.0.0.1:8000/docs',
+      },
+      {
+        source: '/openapi.json',
+        destination: 'http://127.0.0.1:8000/openapi.json',
+      },
+      {
         source: '/api/:path*',
-        destination: 'http://localhost:8000/:path*/',
+        destination: 'http://127.0.0.1:8000/:path*/',
       },
     ]
   },
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ],
+  },
+  /* compiler: {
+    removeConsole: process.env.NODE_ENV === 'production', // Elimina console.log solo en producción
+  }, */
 }
 
 export default nextConfig
