c8a3f6797e9f288d0940da38d79846f84aa90be1
agusasaad <agusasaad1099@hotmail.com>
2025-01-15 16:42:06 -0300

    se agrega logout

--- src/assets/icons/Logout.jsx
+++ src/assets/icons/Logout.jsx
@@ -1,21 +1,26 @@
 const Logout = ({
   width = '24',
   height = '24',
-  color = 'white',
+  color = 'var(--green)',
   className = '',
 }) => (
   <svg
     stroke={color}
     fill={color}
     strokeWidth='0'
-    viewBox='0 0 24 24'
+    viewBox='0 0 512 512'
     height={height}
     width={width}
     xmlns='http://www.w3.org/2000/svg'
     className={className}
   >
-    <path d='m2 12 5 4v-3h9v-2H7V8z'></path>
-    <path d='M13.001 2.999a8.938 8.938 0 0 0-6.364 2.637L8.051 7.05c1.322-1.322 3.08-2.051 4.95-2.051s3.628.729 4.95 2.051 2.051 3.08 2.051 4.95-.729 3.628-2.051 4.95-3.08 2.051-4.95 2.051-3.628-.729-4.95-2.051l-1.414 1.414c1.699 1.7 3.959 2.637 6.364 2.637s4.665-.937 6.364-2.637c1.7-1.699 2.637-3.959 2.637-6.364s-.937-4.665-2.637-6.364a8.938 8.938 0 0 0-6.364-2.637z'></path>
+    <path
+      fill='none'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='32'
+      d='M304 336v40a40 40 0 0 1-40 40H104a40 40 0 0 1-40-40V136a40 40 0 0 1 40-40h152c22.09 0 48 17.91 48 40v40m64 160 80-80-80-80m-192 80h256'
+    ></path>
   </svg>
 )
 
---
+++ src/utils/logout.js
@@ -0,0 +1,19 @@
+export const logout = async () => {
+  await fetch('/api/logout/', {
+    method: 'POST',
+  }).then((res) => {
+    if (res.ok) {
+      try {
+        if (typeof localStorage !== 'undefined') {
+          localStorage.clear()
+        }
+      } catch (error) {
+        console.log(error)
+        alert(error)
+      }
+      window.location.href = '/login'
+    } else {
+      console.log(res)
+    }
+  })
+}
