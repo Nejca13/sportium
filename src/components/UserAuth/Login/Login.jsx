05027c464fed89942ac9d5189e8b7d05eddfddb3
agusasaad <agusasaad1099@hotmail.com>
2025-01-15 12:21:43 -0300

    se agrega login

---
+++ src/components/UserAuth/Login/Login.jsx
@@ -0,0 +1,102 @@
+'use client'
+import { useState } from 'react'
+import styles from './Login.module.css'
+import OpenEye from '@/assets/icons/OpenEye'
+import CloseEye from '@/assets/icons/CloseEye'
+import Link from 'next/link'
+import { login } from '@/services/login/login'
+import { useRouter } from 'next/navigation'
+import Spinner from '@/components/Spinner/Spinner'
+import useStore from '@/app/store'
+
+const Login = () => {
+  const [showPassword, setShowPassword] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
+  const { currentUser, setCurrentUser } = useStore()
+
+  const router = useRouter()
+
+  const handleTogglePassword = () => {
+    setShowPassword((prevState) => !prevState)
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    setIsLoading(true)
+    setErrorMessage('')
+    const formData = Object.fromEntries(new FormData(e.target))
+
+    await login(formData)
+      .then((response) => {
+        if (response.success) {
+          setCurrentUser(response.data)
+          router.push('/')
+        } else {
+          setErrorMessage(response.message.detail)
+        }
+      })
+      .catch((error) => {
+        setErrorMessage('Error al iniciar sesión:', error)
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.container_form}>
+          <form onSubmit={onSubmit}>
+            <label htmlFor='username' id='username'>
+              Correo Electrónico
+              <input
+                type='email'
+                name='username'
+                id='username'
+                placeholder='Correo Electrónico'
+                required
+              />
+            </label>
+            <label htmlFor='password' id='password'>
+              Contraseña
+              <div className={styles.input_container}>
+                <input
+                  type={showPassword ? 'text' : 'password'}
+                  name='password'
+                  id='password'
+                  placeholder='Contraseña'
+                  required
+                />
+                <button
+                  type='button'
+                  className={styles.togglePassword}
+                  onClick={handleTogglePassword}
+                >
+                  {showPassword ? (
+                    <OpenEye width='20px' height='20px' />
+                  ) : (
+                    <CloseEye width='20px' height='20px' />
+                  )}
+                </button>
+              </div>
+            </label>
+
+            <button type='submit' className={styles.button}>
+              {isLoading ? <Spinner /> : 'Ingresar'}
+            </button>
+            {errorMessage && (
+              <small className={styles.error}>{errorMessage}</small>
+            )}
+          </form>
+          <Link href={'/register'} className={styles.link}>
+            ¿No tienes una cuenta? Registrarse
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/services/login/login.js
@@ -0,0 +1,26 @@
+export const login = async (credenciales) => {
+  const credencialesURL = new URLSearchParams(credenciales)
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: credencialesURL,
+  }
+
+  try {
+    const response = await fetch('/api/login/', options)
+
+    if (!response.ok) {
+      const error = await response.json()
+      console.log('Error response:', error)
+      return { success: false, message: error || 'Unknown error' }
+    }
+    const data = await response.json()
+    console.log('Success response:', data)
+    return { success: true, data }
+  } catch (error) {
+    console.log(error)
+    return { success: false, message: error }
+  }
+}
