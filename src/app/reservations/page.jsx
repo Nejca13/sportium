6d659b613696c4c13580b995ad2c6f00a8823861
agusasaad <agusasaad1099@hotmail.com>
2025-01-16 15:44:38 -0300

    Update global styles and add ReservationsPage component

---
+++ src/app/reservations/page.jsx
@@ -0,0 +1,8 @@
+import Reservations from '@/components/Reservations/Reservations'
+import style from './page.module.css'
+
+const ReservationsPage = () => {
+  return <Reservations />
+}
+
+export default ReservationsPage
