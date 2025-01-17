2f0e81d551654938a198d9b7c5c9ab47693f4aa0
agusasaad <agusasaad1099@hotmail.com>
2025-01-17 20:39:59 -0300

    Adjust padding in MyReservations and Reservations components; add logo image to Reservations form with styling

--- src/components/Reservations/Reservations.jsx
+++ src/components/Reservations/Reservations.jsx
@@ -3,6 +3,8 @@ import { useState } from 'react'
 import styles from './Reservations.module.css'
 import StepOne from './StepReservation/StepOne'
 import StepTwo from './StepReservation/StepTwo'
+import imagen_logo from '@/assets/images/logos/Recurso 14_023535.png'
+import Image from 'next/image'
 
 const Reservations = () => {
   const [isLoading, setIsLoading] = useState(false)
@@ -10,8 +12,20 @@ const Reservations = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.content}>
+      <div
+        className={styles.content}
+        style={step === 1 ? { paddingTop: '200px' } : { paddingTop: '150px' }}
+      >
         <div className={styles.container_form}>
+          {step === 1 && (
+            <Image
+              src={imagen_logo}
+              alt='logo'
+              width={200}
+              height={200}
+              className={styles.logo}
+            />
+          )}
           {step === 1 && (
             <StepOne
               setStep={setStep}
