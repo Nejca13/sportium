809084ed5bd7eca8d0b0d12ff089905a666e706d
Nejca13 <nicolascontreras677@gmail.com>
2025-01-20 15:57:41 -0300

    Add user activation feature: implement email notification and activation endpoint for new users

--- src/app/api/routers/users.py
+++ src/app/api/routers/users.py
@@ -2,9 +2,14 @@ from datetime import datetime
 import json
 from beanie import PydanticObjectId
 from fastapi import APIRouter, File, Form, HTTPException, UploadFile
+from app.api.init_db import BASE_URL
 from app.api.models.user import User, UserResponse, UserUpdate
 from app.api.utils.hash_password.def_hash_password import hash_password
 from app.api.utils.save_image import save_image
+from app.api.utils.send_email import send_email
+
+from fastapi import HTTPException, UploadFile, File, Form
+from beanie import PydanticObjectId
 
 
 router = APIRouter(prefix="/users", tags=["Usuarios"])
@@ -51,6 +56,12 @@ async def create_user(user: str = Form(...), image: UploadFile = File(...)):
     # Crea el usuario en la base de datos
     try:
         new_user = await User(**user_data).create()
+
+        await send_email(
+            destinatario=new_user.email,
+            asunto="Bienvenido a la plataforma",
+            mensaje=f"¡Bienvenido, {new_user.name} {new_user.last_name}! Te damos la bienvenida a la plataforma. /n Click en el siguiente enlace para activar tu cuenta: {BASE_URL}/api/users/{new_user.id}/activate/",
+        )
         return new_user
     except Exception as e:
         raise HTTPException(
@@ -58,8 +69,26 @@ async def create_user(user: str = Form(...), image: UploadFile = File(...)):
         )
 
 
-from fastapi import HTTPException, UploadFile, File, Form
-from beanie import PydanticObjectId
+# Marcar usuario como activo
+@router.patch("/{user_id}/activate/", response_model=UserResponse)
+async def activate_user(user_id: str):
+    try:
+        user_id = PydanticObjectId(user_id)
+    except Exception:
+        raise HTTPException(status_code=400, detail="ID de usuario inválido")
+
+    # Busca el usuario en la base de datos
+    user = await User.get(user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+
+    # Marca el usuario como activo
+    user.is_active = True
+
+    # Guarda los cambios en la base de datos
+    await user.save()
+
+    return user
 
 
 @router.patch("/{user_id}/", response_model=UserResponse)
