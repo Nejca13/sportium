67c524bbe2046ccb3d1ede50f89a81d4cb5e392d
agusasaad <agusasaad1099@hotmail.com>
2025-01-27 15:13:36 -0300

    Refactor Login component for improved readability; update CSS for consistent styling

--- src/components/UserAuth/Login/Login.jsx
+++ src/components/UserAuth/Login/Login.jsx
@@ -1,132 +1,132 @@
-'use client'
-import { useEffect, useState } from 'react'
-import styles from './Login.module.css'
-import OpenEye from '@/assets/icons/OpenEye'
-import CloseEye from '@/assets/icons/CloseEye'
-import Link from 'next/link'
-import { login } from '@/services/login/login'
-import { useRouter } from 'next/navigation'
-import Spinner from '@/components/Spinner/Spinner'
-import useStore from '@/app/store'
-import imagen_logo from '@/assets/images/logos/Recurso 14_023535.png'
-import Image from 'next/image'
-import ArrowLeft from '@/assets/icons/ArrowLeft'
-import Alert from '@/assets/icons/Alert'
-import Recovery from './Recovery/Recovery'
+"use client";
+import { useEffect, useState } from "react";
+import styles from "./Login.module.css";
+import OpenEye from "@/assets/icons/OpenEye";
+import CloseEye from "@/assets/icons/CloseEye";
+import Link from "next/link";
+import { login } from "@/services/login/login";
+import { useRouter } from "next/navigation";
+import Spinner from "@/components/Spinner/Spinner";
+import useStore from "@/app/store";
+import imagen_logo from "@/assets/images/logos/Recurso 14_023535.png";
+import Image from "next/image";
+import ArrowLeft from "@/assets/icons/ArrowLeft";
+import Alert from "@/assets/icons/Alert";
+import Recovery from "./Recovery/Recovery";
 
 const Login = () => {
-  const [showPassword, setShowPassword] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
-  const [errorMessage, setErrorMessage] = useState('')
-  const [showRecovery, setShowRecovery] = useState(false)
-  const { currentUser, setCurrentUser } = useStore()
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [showRecovery, setShowRecovery] = useState(false);
+  const { currentUser, setCurrentUser } = useStore();
 
-  const router = useRouter()
+  const router = useRouter();
 
   const handleTogglePassword = () => {
-    setShowPassword((prevState) => !prevState)
-  }
+    setShowPassword((prevState) => !prevState);
+  };
 
   const onSubmit = async (e) => {
-    e.preventDefault()
-    setIsLoading(true)
-    setErrorMessage('')
-    const formData = Object.fromEntries(new FormData(e.target))
+    e.preventDefault();
+    setIsLoading(true);
+    setErrorMessage("");
+    const formData = Object.fromEntries(new FormData(e.target));
 
     await login(formData)
       .then((response) => {
         if (response.success) {
-          console.log(response.data.user.is_verified)
+          console.log(response.data.user.is_verified);
           if (response.data.user.is_verified === false) {
             setErrorMessage(
-              'El correo no ha sido verificado, revisa tu bandeja de entrada o carpeta de spam'
-            )
+              "El correo no ha sido verificado, revisa tu bandeja de entrada o carpeta de spam"
+            );
           } else {
-            setCurrentUser(response.data)
-            router.push('/')
+            setCurrentUser(response.data);
+            router.push("/");
           }
         } else {
-          setErrorMessage(response.message.detail)
+          setErrorMessage(response.message.detail);
         }
       })
       .catch((error) => {
-        setErrorMessage('Error al iniciar sesión:', error)
+        setErrorMessage("Error al iniciar sesión:", error);
       })
       .finally(() => {
-        setIsLoading(false)
-      })
-  }
+        setIsLoading(false);
+      });
+  };
   useEffect(() => {
     if (currentUser) {
-      router.push('/')
+      router.push("/");
     }
-  }, [currentUser, router])
+  }, [currentUser, router]);
 
   return (
     <div className={styles.container}>
       <div className={styles.back_button}>
-        <Link href={'/'}>
+        <Link href={"/"}>
           <ArrowLeft />
           Volver al inicio
         </Link>
       </div>
       <div className={styles.content}>
         <div className={styles.container_form}>
-          <Image src={imagen_logo} alt='logo' width={200} height={200} />
+          <Image src={imagen_logo} alt="logo" width={200} height={200} />
           {!showRecovery ? (
             <>
               <form onSubmit={onSubmit}>
-                <label htmlFor='username' id='username'>
+                <label htmlFor="username" id="username">
                   Correo Electrónico
                   <input
-                    type='email'
-                    name='username'
-                    id='username'
-                    placeholder='Correo Electrónico'
+                    type="email"
+                    name="username"
+                    id="username"
+                    placeholder="Correo Electrónico"
                     required
-                    autoComplete='email'
+                    autoComplete="email"
                   />
                 </label>
-                <label htmlFor='password' id='password'>
+                <label htmlFor="password" id="password">
                   Contraseña
                   <div className={styles.input_container}>
                     <input
-                      type={showPassword ? 'text' : 'password'}
-                      name='password'
-                      id='password'
-                      placeholder='Contraseña'
+                      type={showPassword ? "text" : "password"}
+                      name="password"
+                      id="password"
+                      placeholder="Contraseña"
                       required
                     />
                     <button
-                      type='button'
+                      type="button"
                       className={styles.togglePassword}
                       onClick={handleTogglePassword}
                     >
                       {showPassword ? (
-                        <OpenEye width='20px' height='20px' />
+                        <OpenEye width="20px" height="20px" />
                       ) : (
-                        <CloseEye width='20px' height='20px' />
+                        <CloseEye width="20px" height="20px" />
                       )}
                     </button>
                   </div>
-                  <button
-                    className={styles.recovery_button}
-                    onClick={(e) => {
-                      e.preventDefault(), setShowRecovery(true)
-                    }}
-                  >
-                    ¿Olvidaste tu contraseña?
-                  </button>
                 </label>
 
-                <button type='submit' className={styles.button}>
-                  {isLoading ? <Spinner /> : 'Ingresar'}
+                <button type="submit" className={styles.button}>
+                  {isLoading ? <Spinner /> : "Ingresar"}
                 </button>
                 {errorMessage && (
                   <small className={styles.error}>{errorMessage}</small>
                 )}
               </form>
-              <Link href={'/register'} className={styles.link}>
+              <button
+                className={styles.recovery_button}
+                onClick={(e) => {
+                  setShowRecovery(true);
+                }}
+              >
+                ¿Olvidaste tu contraseña?
+              </button>
+              <Link href={"/register"} className={styles.link}>
                 ¿No tienes una cuenta? Registrarse
               </Link>
             </>
@@ -136,7 +136,7 @@ const Login = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
